ca39c21da6069c470be27c32fc5cc488db9a6e7c
DUNNSEAN <dunnin23@gmail.com>
2024-08-15 19:43:50 -0500

    [Sprint 3][Task 5] feat: Implement banned token store

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -7,25 +7,33 @@ use crate::{
     utils::{auth::validate_token, constants::JWT_COOKIE_NAME},
 };
 
-pub async fn logout(jar: CookieJar) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
-    // Retrieve JWT cookie from the `CookieJar`
-    // Return AuthAPIError::MissingToken is the cookie is not found
+pub async fn logout(
+    State(state): State<AppState>,
+    jar: CookieJar,
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
     let cookie = match jar.get(JWT_COOKIE_NAME) {
         Some(cookie) => cookie,
         None => return (jar, Err(AuthAPIError::MissingToken)),
     };
 
-
+    // Validate token
     let token = cookie.value().to_owned();
-
-    // Validate JWT token by calling `validate_token` from the auth service.
-    // If the token is valid you can ignore the returned claims for now.
-    // Return AuthAPIError::InvalidToken is validation fails.
-    let token = cookie.value().to_owned();
-    let _ = match validate_token(&token).await {
+    let _ = match validate_token(&token, state.banned_token_store.clone()).await {
         Ok(claims) => claims,
         Err(_) => return (jar, Err(AuthAPIError::InvalidToken)),
     };
+
+    // Add token to banned list
+    if state
+        .banned_token_store
+        .write()
+        .await
+        .add_token(token.to_owned())
+        .await
+        .is_err()
+    {
+        return (jar, Err(AuthAPIError::UnexpectedError));
+    }
 
     // Remove jwt cookie
     let jar = jar.remove(cookie::Cookie::from(JWT_COOKIE_NAME));
--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -5,7 +5,10 @@ use uuid::Uuid;
 use reqwest::cookie::Jar;
 
 use auth_service::{
-    app_state::AppState, services::hashmap_user_store::HashmapUserStore, utils::constants::test,
+    app_state::{AppState, BannedTokenStoreType},
+    services::{
+        hashmap_user_store::HashmapUserStore, hashset_banned_token_store::HashsetBannedTokenStore,
+    }, utils::constants::test,
     Application,
 };
 
@@ -13,6 +16,7 @@ pub struct TestApp {
     pub address: String,
     pub cookie_jar: Arc<Jar>, // New!
     pub http_client: reqwest::Client,
+    pub banned_token_store: BannedTokenStoreType,
 }
 
 
@@ -20,7 +24,9 @@ pub struct TestApp {
 impl TestApp {
     pub async fn new() -> Self {
         let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
-        let app_state = AppState::new(user_store);
+
+        let banned_token_store = Arc::new(RwLock::new(HashsetBannedTokenStore::default()));
+        let app_state = AppState::new(user_store, banned_token_store.clone());
 
         let app = Application::build(app_state, test::APP_ADDRESS)
         .await
@@ -39,6 +45,7 @@ impl TestApp {
         Self {
             address,
             cookie_jar,
+            banned_token_store,
             http_client,
         }
     }
